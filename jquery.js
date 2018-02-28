a0c8cb758d1c025c14f739f94d7d48890d2aac68
Ranjith Acharya <ranjithacharya997@gmail.com>
2018-02-28 13:24:32 +0530

    Add Task Name, with Date, & Description of the Task with changes in the interface with the user.

--- jquery.js
+++ jquery.js
@@ -1,44 +1,62 @@
-var count = 0;
-var count1 = 0;
+var count=0;
 $(document).ready(function(){
-	$("#btn-submit").click(function(){
-		var task = $("#taskname").val();
-		if(task == ""){
-			swal("Please Specify Task Name","","error");
-			$("#taskname").css({"border":"1px solid #cd201f"});
-			$("#taskname").val("");
-			$("#taskname").focus();
+	$("#btnSubmit").click(function(){
+		var NewTask = $("#taskName").val();
+		var NewDate = $("#taskDate").val();
+		var NewDesc = $("#taskDesc").val();
+		if(NewTask==""){
+			swal({title:"",text:"Enter Task Name",type:"error",showConfirmButton:false,timer:2000});
+			$("#taskName").css({"border":"1px solid #cd201f"});
+			$("#taskName").focus();
 			return false;
-		}else{
-			//$("#task").append(task);
-			var tk_id = "tk"+count;
-			count++;
-			var divBody = "<div class='card col-md-6' id='"+tk_id+"' style='margin:0 auto;margin-bottom:25px;'><div class='card-body'><input type='checkbox' onclick='move(this)'>&nbsp;&nbsp;"+$("#taskname").val()+"</div></div>";
-			$("#task").append(divBody);
-			$("#taskname").val("");
 		}
-	});
-	$("#btn-reset").click(function(){
-		$("#taskname").val("");
-		$("#taskname").css({"border":"1px solid #ccc"});
+		if(NewDate==""){
+			swal({title:"",text:"Select Task Date",type:"error",showConfirmButton:false,timer:2000});
+			$("#taskDate").css({"border":"1px solid #cd201f"});
+			$("#taskDate").focus();
+			return false;
+		}
+		if(NewDesc==""){
+			swal({title:"",text:"Describe About your Task",type:"error",showConfirmButton:false,timer:2000});
+			$("#taskDesc").css({"border":"1px solid #cd201f"});
+			$("#taskDesc").focus();
+			return false;
+		}
+		else{
+			$("#taskName").val("");
+			$("#taskName").css({"border":"1px solid #ccc"});
+			$("#taskDate").val("");
+			$("#taskDate").css({"border":"1px solid #ccc"});
+			$("#taskDesc").val("");
+			$("#taskDesc").css({"border":"1px solid #ccc"});
+			var pend_id = "tk"+count;
+			count++;
+			var taskDiv = "<div class='card' id='"+pend_id+"'><div class='card-body'><input type='checkbox' onclick='move(this)'>&nbsp;&nbsp;"+NewDate+";</br>Task Name : "+NewTask+".</br>Task Description : "+NewDesc+".</div></div></br>";
+			$("#pend_task").append(taskDiv);
+		}
+});
+	$("#btnReset").click(function(){
+		$("#taskName").css({"border":"1px solid #ccc"});
 	});
 });
+
 function move(a){
-	var move_task = $(a).parent().parent().attr("id");
-	var temp = $(a).parent().text();
-	swal({title:"The Task Name -"+temp});
+	var move_taskid = $(a).parent().parent().attr("id");
+	var move_taskName = $(a).parent().text();
+	//alert(move_taskid);
+	//alert(move_taskName);
+	swal({title:"",text:"Task Has Been Moved to Completed Task",type:"success",showConfirmButton:false,timer:2000});
+	var comp_taskid = "compTask"+count;
+	count++;
+	var comp_taskDiv = "<div class='card' id='"+comp_taskid+"'><div class='card-body'>"+move_taskName+"<button class='close' onclick='remove(this)'>&times;</button></div></div>";
+	$("#comp_task").append(comp_taskDiv);
 	$(a).parent().parent().remove();
-	//alert("The Task Name :"+temp);
-	//alert(move_task);
-	var tk1_id = "tk"+count1;
-	count1++;
-	var divbody = "<div class='card col-md-6' id='"+tk1_id+"' style='margin:0 auto;margin-bottom:25px;'><div class='card-body'>"+temp+"<button class='close' onclick='remove(this)'>&times;</button></div></div>";
-	$("#complete").append(divbody);
 }
+
 function remove(b){
-	var task_done = $(b).parent().parent().attr("id");
-	var task_done_name = $(b).parent().text();
-	//alert(task_done);
-	swal({title:"The Task -"+task_done_name+"is Completely Removed Successfully"});
+	var done_task = $(b).parent().parent().attr("id");
+	swal({title:"",text:"The Task Has Been Removed Successfully",type:"success",showConfirmButton:false,timer:2000});
+	//alert(done_task);
 	$(b).parent().parent().remove();
+	
 }
